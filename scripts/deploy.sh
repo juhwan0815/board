5e7176f1d03a4283495a1a0ed2f6f522cb0201d0
juhwan0815 <juwom0831@naver.com>
2022-01-23 15:56:50 +0900

    배포 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,13 @@ echo rjatjd0815* | docker login -u juhwan0815 --password-stdin
 sudo docker ps -a -q --filter "name=board" | grep -q . && docker stop board && docker rm board | true
 
 # 기존 이미지 삭제
-sudo docker rmi juhwan/board:1.0
+sudo docker rmi juhwan0815/board:1.0
 
 # 도커허브 이미지 pull
-sudo docker pull juhwan/board:1.0
+sudo docker pull juhwan0815/board:1.0
 
 # 도커 run
-docker run -d -p 8080:8080 --name board juhwan/board:1.0
+docker run -d -p 8080:8080 --name board juhwan0815/board:1.0
 
 
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
