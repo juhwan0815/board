e1efe7c60c605b46cad8b2487bd4d98323b3a6f0
juhwan0815 <juwom0831@naver.com>
2022-01-23 15:25:52 +0900

    배포 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,5 +10,6 @@ sudo docker pull juhwan/board:1.0
 # 도커 run
 docker run -d -p 8080:8080 --name board juhwan/board:1.0
 
+
 # 사용하지 않는 불필요한 이미지 삭제 -> 현재 컨테이너가 물고 있는 이미지는 삭제되지 않습니다.
 docker rmi -f $(docker images -f "dangling=true" -q) || true
